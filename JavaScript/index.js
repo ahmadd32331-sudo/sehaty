126fad74fe1eb9f1088dbb0e025f8674e598b160
ahmadd32331-sudo <ahmadd32331@gmail.com>
2026-02-02 03:59:29 +0300

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -20,15 +20,15 @@ document.getElementById('leaveForm').addEventListener('submit', function(e) {
         <tr><th>اسم المنشأة</th><td>مركز طويق الغربي</td></tr>
       </table>
     `;
-  } else if (code === 'GSL23030900510' && id === '1132032499') {
+  } else if (code === 'GSL23030675421' && id === '1117471217') {
     resultContainer.innerHTML = `
       <table class="result-table">
-        <tr><th>الاسم</th><td>عمر عيسى الحربي</td></tr>
-        <tr><th>رقم الهوية</th><td>1132032499</td></tr>
-        <tr><th>تاريخ إصدار تقرير الإجازة</th><td>2025-11-16</td></tr>
-        <tr><th>تاريخ بداية الإجازة</th><td>2025-11-16</td></tr>
-        <tr><th>تاريخ نهاية الإجازة</th><td>2025-11-17</td></tr>
-        <tr><th>مدة الإجازة</th><td>يومين</td></tr>
+        <tr><th>الاسم</th><td>حمود عيسى عبدالله الحربي</td></tr>
+        <tr><th>رقم الهوية</th><td>1117471217</td></tr>
+        <tr><th>تاريخ إصدار تقرير الإجازة</th><td>2026-01-27</td></tr>
+        <tr><th>تاريخ بداية الإجازة</th><td>2026-01-27</td></tr>
+        <tr><th>تاريخ نهاية الإجازة</th><td>2026-01-27</td></tr>
+        <tr><th>مدة الإجازة</th><td>يوم واحد</td></tr>
         <tr><th>اسم المنشأة</th><td>مركز طويق الغربي</td></tr>
       </table>
     `;
@@ -36,5 +36,6 @@ document.getElementById('leaveForm').addEventListener('submit', function(e) {
     alert('رقم الخدمة أو رقم الهوية غير صحيح');
   }
 });
+
 
 
